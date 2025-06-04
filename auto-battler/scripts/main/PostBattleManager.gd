fdac189a9d1766d72333910c60f08b9da745d722
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-04 08:35:28 -0700

    Refactor PostBattleSummary layout

--- auto-battler/scripts/main/PostBattleManager.gd
+++ auto-battler/scripts/main/PostBattleManager.gd
@@ -3,7 +3,7 @@ extends Node
 signal post_battle_complete
 
 func _ready():
-    $VBox/ContinueButton.connect("pressed", self, "_on_Continue_pressed")
+    $MainVBox/ContinueButton.connect("pressed", self, "_on_Continue_pressed")
 
 func _on_Continue_pressed():
     emit_signal("post_battle_complete")
