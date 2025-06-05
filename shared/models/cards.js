7d0674abc32c34ec9e73124c7e15ac11d02664a6
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-05 10:49:23 -0700

    feat(ui): add PartyBuilder component

---
+++ shared/models/cards.js
@@ -0,0 +1,8 @@
+export const sampleCards = [
+  { id: 'strike', name: 'Strike' },
+  { id: 'heal', name: 'Heal' },
+  { id: 'fireball', name: 'Fireball' },
+  { id: 'guard', name: 'Guard' },
+  { id: 'poison', name: 'Poison' },
+  { id: 'arrow', name: 'Arrow Shot' },
+]
