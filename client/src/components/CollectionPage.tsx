b4cb3e731d42ab5577380c57ef7838879bf0b7a2
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-06 13:04:31 -0700

    Add Town hub navigation and feature routes

---
+++ client/src/components/CollectionPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CardCollection from './CardCollection.tsx'
+import styles from './TownFeaturePage.module.css'
+
+export default function CollectionPage() {
+  return (
+    <div className={styles.container}>
+      <Link to="/town" className={styles.back}>Back to Town</Link>
+      <CardCollection />
+    </div>
+  )
+}
