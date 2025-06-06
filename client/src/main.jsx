4f64ecaa52633c0cb8a6c07f89458564cae262ab
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-06 07:14:26 -0700

    fix routing context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { useGameStore } from './store/gameStore'
@@ -13,11 +14,13 @@ useGameStore.getState().load()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GameStateProvider>
-      <ModalProvider>
-        <NotificationProvider>
-          <App />
-        </NotificationProvider>
-      </ModalProvider>
+      <BrowserRouter>
+        <ModalProvider>
+          <NotificationProvider>
+            <App />
+          </NotificationProvider>
+        </ModalProvider>
+      </BrowserRouter>
     </GameStateProvider>
   </StrictMode>,
 )
