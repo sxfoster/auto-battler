f76149ed3d206a9e26d9d17ce4ce2bb9f6ed9855
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-06 18:51:19 -0700

    feat(game): add dungeon grid scene

--- game/src/scenes/TownScene.js
+++ game/src/scenes/TownScene.js
@@ -1,21 +1,25 @@
-import Phaser from 'phaser'
-import { loadGameState, saveGameState } from '../state'
+import Phaser from "phaser";
+import { loadGameState, saveGameState } from "../state";
+import { generateDungeon } from "shared/dungeonState";
 
 export default class TownScene extends Phaser.Scene {
   constructor() {
-    super('town')
+    super("town");
   }
 
   create() {
-    this.add.text(400, 300, 'Town: press SPACE to enter dungeon', {
-      fontSize: '20px',
-    }).setOrigin(0.5)
+    this.add
+      .text(400, 300, "Town: press SPACE to enter dungeon", {
+        fontSize: "20px",
+      })
+      .setOrigin(0.5);
 
-    this.input.keyboard.once('keydown-SPACE', () => {
-      const state = loadGameState()
-      state.location = 'dungeon'
-      saveGameState(state)
-      this.scene.start('dungeon')
-    })
+    this.input.keyboard.once("keydown-SPACE", () => {
+      generateDungeon(5, 5);
+      const state = loadGameState();
+      state.location = "dungeon";
+      saveGameState(state);
+      this.scene.start("dungeon");
+    });
   }
 }
