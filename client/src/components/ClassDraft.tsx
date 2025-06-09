0ed06beef2e738d8ce4b6978ea024a2814e985c9
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-09 15:54:20 -0700

    Fix ClassDraft mock data import path

--- client/src/components/ClassDraft.tsx
+++ client/src/components/ClassDraft.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { UnitState } from '../../../shared/models/UnitState';
-import { MOCK_HEROES } from '@shared/mock-data';
+import { MOCK_HEROES } from '../../../game/src/logic/mock-data.js';
 
 interface Props {
   onComplete: (party: UnitState[]) => void;
