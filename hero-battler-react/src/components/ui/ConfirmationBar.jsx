c19fd64255d0e92479a4acbca0e4ced87455a15b
Stephan Foster <stephan.foster@simplot.com>
2025-06-12 18:53:08 -0700

    restart

--- hero-battler-react/src/components/ui/ConfirmationBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ConfirmationBar({ onConfirm }) {
-  return (
-    <div className="confirmation-bar visible">
-      <button className="confirm-button" onClick={onConfirm}>Start Battle</button>
-    </div>
-  );
-}
