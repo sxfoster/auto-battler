f034aea61b1df84832b6a458b1da765bc21715e9
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-06-26 17:49:40 -0700

    Update who command to search by name

--- discord-bot/commands/who.js
+++ discord-bot/commands/who.js
@@ -3,23 +3,28 @@ const userService = require('../src/utils/userService');
 
 const data = new SlashCommandBuilder()
   .setName('who')
-  .setDescription('Display your current class');
+  .setDescription('Look up a player by name')
+  .addStringOption(option =>
+    option.setName('player')
+      .setDescription('Player name')
+      .setRequired(true)
+  );
 
 async function execute(interaction) {
-  try {
-    const user = await userService.getUser(interaction.user.id);
-    if (!user) {
-      await interaction.reply({ content: 'User not found. Try /game to get started.', ephemeral: true });
-      return;
-    }
-    if (!user.class) {
-      await interaction.reply({ content: 'You have not chosen a class yet.', ephemeral: true });
-      return;
-    }
-    await interaction.reply({ content: `You are a **${user.class}**.` });
-  } catch (err) {
-    await interaction.reply({ content: 'Failed to look up user.', ephemeral: true });
+  const searchName = interaction.options.getString('player');
+  const user = await userService.getUserByName(searchName);
+
+  if (!user) {
+    await interaction.reply({ content: `Could not find a player named ${searchName}.`, ephemeral: true });
+    return;
   }
+
+  if (!user.class) {
+    await interaction.reply({ content: `${user.name} has not yet chosen a class.` });
+    return;
+  }
+
+  await interaction.reply({ content: `${user.name} - ${user.class}` });
 }
 
 module.exports = { data, execute };
