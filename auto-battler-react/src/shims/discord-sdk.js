8c3d177aa8bc591ef5c4c03d73a157bf3e52fa5d
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-07-03 09:15:23 -0700

    Add Discord SDK stub and alias in Vite config

---
+++ auto-battler-react/src/shims/discord-sdk.js
@@ -0,0 +1,9 @@
+// stub for @discord/embedded-app-sdk
+export class DiscordSDK {
+  constructor(_clientId) {
+    console.warn('DiscordSDK stub initialized');
+  }
+  ready() {
+    return Promise.resolve();
+  }
+}
