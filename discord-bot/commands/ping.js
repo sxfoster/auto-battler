105909dd7bda1a28ff0666e4ee5164fc634fa1a5
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-07-07 17:10:09 -0700

    Update ping command to use ephemeral option

--- discord-bot/commands/ping.js
+++ discord-bot/commands/ping.js
@@ -1,13 +1,12 @@
-const { MessageFlags } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
 const { simple } = require('../src/utils/embedBuilder');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    async execute(interaction) {
-        const embed = simple('Pong!');
-        await interaction.reply({ embeds: [embed], flags: [MessageFlags.Ephemeral] });
-    },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    const embed = simple('Pong!');
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  },
 };
