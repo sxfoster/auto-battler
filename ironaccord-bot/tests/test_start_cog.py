15c7373c678cc6168b530f305e61f2660082ada9
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-07-08 18:43:29 -0700

    Add interactive adventure tutorial

--- ironaccord-bot/tests/test_start_cog.py
+++ ironaccord-bot/tests/test_start_cog.py
@@ -4,36 +4,48 @@ discord = pytest.importorskip("discord")
 from discord.ext import commands
 from ironaccord_bot.cogs import start
 
-class DummyFollowup:
+class DummyResponse:
     def __init__(self):
-        self.args = None
+        self.called = False
         self.kwargs = None
-    async def send(self, *args, **kwargs):
-        self.args = args
+
+    async def send_message(self, *args, **kwargs):
+        self.called = True
         self.kwargs = kwargs
 
-class DummyResponse:
-    async def defer(self, *args, **kwargs):
-        pass
+class DummyMessage:
+    pass
 
 class DummyInteraction:
     def __init__(self):
         self.user = type("User", (), {"id": 1, "name": "Test", "display_name": "Test"})()
         self.response = DummyResponse()
-        self.followup = DummyFollowup()
+        self.message = DummyMessage()
+
+    async def original_response(self):
+        return self.message
 
 @pytest.mark.asyncio
 async def test_start_cog_returns_view(monkeypatch):
     bot = commands.Bot(command_prefix="!", intents=discord.Intents.none())
     cog = start.StartCog(bot)
 
-    def fake_query(self, prompt, max_tokens=200, **kwargs):
-        return "intro"
+    called = {}
 
-    monkeypatch.setattr(start.MixtralAgent, "query", fake_query, raising=False)
+    class DummyView(start.AdventureView):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            called["created"] = True
+
+        async def _handle_next_phase(self, interaction):
+            called["interaction"] = interaction
+
+    monkeypatch.setattr(start, "AdventureView", DummyView)
     interaction = DummyInteraction()
 
     await cog.start.callback(cog, interaction)
 
-    view = interaction.followup.kwargs.get("view")
-    assert isinstance(view, start.SimpleTutorialView)
+    assert interaction.response.called
+    assert interaction.response.kwargs.get("ephemeral") is True
+    assert called.get("created")
+    assert called.get("interaction") is interaction.message
