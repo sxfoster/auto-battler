f276461d5eb228b1368e3db5e4f6ceab03983983
Stephan Foster <1588616+sxfoster@users.noreply.github.com>
2025-07-18 14:21:01 -0700

    Refactor start command to use new quiz system

--- ironaccord_bot/services/__init__.py
+++ ironaccord_bot/services/__init__.py
@@ -1,7 +1,6 @@
 from .ollama_service import OllamaService
 from .rag_service import RAGService
 from .player_context_service import gather_player_context
-from .background_quiz_service import BackgroundQuizService
 from .mission_engine_service import MissionEngineService
 from .quiz_content_service import QuizContentService
 from .quiz_service import QuizService
@@ -10,7 +9,6 @@ __all__ = [
     "OllamaService",
     "RAGService",
     "gather_player_context",
-    "BackgroundQuizService",
     "MissionEngineService",
     "QuizContentService",
     "QuizService",
